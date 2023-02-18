4e8d668ad3cc16d317f42526f715e81d2307095e
caozhanhao <cao2013zh@163.com>
2023-02-18 14:57:58 +0800

    bugfix.

--- include/internal/term.h
+++ include/internal/term.h
@@ -51,54 +51,14 @@ namespace czh::term
   public:
     struct termios initial_settings, new_settings;
     int peek_character;
-    KeyBoard()
-    {
-      tcgetattr(0, &initial_settings);
-      new_settings = initial_settings;
-      new_settings.c_lflag &= ~ICANON;
-      new_settings.c_lflag &= ~ECHO;
-      new_settings.c_lflag &= ~ISIG;
-      new_settings.c_cc[VMIN] = 1;
-      new_settings.c_cc[VTIME] = 0;
-      tcsetattr(0, TCSANOW, &new_settings);
-      peek_character = -1;
-    }
+    KeyBoard();
 
-    ~KeyBoard()
-    {
-      tcsetattr(0, TCSANOW, &initial_settings);
-    }
+    ~KeyBoard();
 
-    int kbhit()
-    {
-      unsigned char ch;
-      int nread;
-      if (peek_character != -1) return 1;
-      new_settings.c_cc[VMIN] = 0;
-      tcsetattr(0, TCSANOW, &new_settings);
-      nread = read(0, &ch, 1);
-      new_settings.c_cc[VMIN] = 1;
-      tcsetattr(0, TCSANOW, &new_settings);
+    int kbhit();
 
-      if (nread == 1) {
-        peek_character = ch;
-        return 1;
-      }
-      return 0;
-    }
-
-    int getch() {
-      char ch;
-
-      if (peek_character != -1) {
-        ch = peek_character;
-        peek_character = -1;
-      }
-      else read(0, &ch, 1);
-      return ch;
-    }
+    int getch();
   };
-  KeyBoard keyboard;
 #endif
   
   int getch();
--- src/internal/term.cpp
+++ src/internal/term.cpp
@@ -29,6 +29,55 @@
 
 namespace czh::term
 {
+#if defined(__linux__)
+  KeyBoard::KeyBoard()
+    {
+      tcgetattr(0, &initial_settings);
+      new_settings = initial_settings;
+      new_settings.c_lflag &= ~ICANON;
+      new_settings.c_lflag &= ~ECHO;
+      new_settings.c_lflag &= ~ISIG;
+      new_settings.c_cc[VMIN] = 1;
+      new_settings.c_cc[VTIME] = 0;
+      tcsetattr(0, TCSANOW, &new_settings);
+      peek_character = -1;
+    }
+  
+  KeyBoard::~KeyBoard()
+    {
+      tcsetattr(0, TCSANOW, &initial_settings);
+    }
+    
+    int KeyBoard::kbhit()
+    {
+      unsigned char ch;
+      int nread;
+      if (peek_character != -1) return 1;
+      new_settings.c_cc[VMIN] = 0;
+      tcsetattr(0, TCSANOW, &new_settings);
+      nread = read(0, &ch, 1);
+      new_settings.c_cc[VMIN] = 1;
+      tcsetattr(0, TCSANOW, &new_settings);
+      
+      if (nread == 1) {
+        peek_character = ch;
+        return 1;
+      }
+      return 0;
+    }
+    
+    int KeyBoard::getch() {
+      char ch;
+      
+      if (peek_character != -1) {
+        ch = peek_character;
+        peek_character = -1;
+      }
+      else read(0, &ch, 1);
+      return ch;
+    }
+  KeyBoard keyboard;
+#endif
   int getch()
   {
 #if defined(_WIN32) || defined(_WIN64)
